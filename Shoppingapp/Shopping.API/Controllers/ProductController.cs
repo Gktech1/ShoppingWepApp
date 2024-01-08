20be83e75f20b939deea7168878f3861c566f5ba
Olukunle Edun <favourblessing1@gmail.com>
2024-01-08 13:49:42 +0100

    features updates on Shopping API Service

---
+++ Shoppingapp/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+            private readonly ILogger<ProductController> _logger;
+
+            public ProductController(ILogger<ProductController> logger)
+            {
+                _logger = logger;
+            }
+
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Product> Get()
+        {
+           var products = ProductContext.Products;
+            return products;
+        }
+        
+       /* [HttpGet(Name = "GetProducts")]
+            public IEnumerable<Product> Get()
+            {
+                return Enumerable.Range(1, 5).Select(index => new Product
+                {
+                    Name = "gkt"
+                })
+                .ToArray();
+            }*/
+        
+    }
+}
+
